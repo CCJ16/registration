1900ead46c42551660025a0f09bc6e0ffc587a58
Matthew Dawson <matthew@mjdsystems.ca>
2015-01-11 15:07:53 -0500

    Change the tests to better use GoConvey.
    Better use GoConvey to make the tests easier to read/write.

--- regbackend/reg_info_test.go
+++ regbackend/reg_info_test.go
@@ -20,47 +20,62 @@ func (db *testPreRegDb) CreateRecord(in GroupPreRegistration) error {
 }
 
 func TestPreRegCreateRequest(t *testing.T) {
-	goodRecord := GroupPreRegistration{}
-	goodRecordBody := bytes.Buffer{}
-	if bytes, err := json.Marshal(goodRecord); err != nil {
-		t.Fatal(err)
-	} else {
-		goodRecordBody.Write(bytes)
-	}
-
-	prdb := &testPreRegDb{}
-	prh := PreRegHandler{
-		db: prdb,
-	}
-
-	Convey("Should return 201 and insert for a good record", t, func() {
-		r, err := http.NewRequest("POST", "http://localhost:8080/prereg", &goodRecordBody)
-		if err != nil {
+	Convey("Starting with a Group Pre Registration handler", t, func() {
+		goodRecord := GroupPreRegistration{
+			PackName:  "Pack A",
+			GroupName: "Test Group",
+			Council:   "1st Testingway",
+		}
+		goodRecordBody := bytes.Buffer{}
+		if bytes, err := json.Marshal(goodRecord); err != nil {
 			t.Fatal(err)
-		}
-		w := httptest.NewRecorder()
-
-		prh.Create(w, r)
-
-		if w.Code != 201 {
-			t.Errorf("Did not receive a 201 response for record creation, got: %v", w.Code)
+		} else {
+			goodRecordBody.Write(bytes)
 		}
 
-		if len(prdb.entries) != 1 || prdb.entries[0] != goodRecord {
-			t.Errorf("Failed to insert record!")
+		prdb := &testPreRegDb{}
+		prh := PreRegHandler{
+			db: prdb,
 		}
-	})
-	Convey("Should return 400 for invalid json", t, func() {
-		r, err := http.NewRequest("POST", "http://localhost:8080/prereg", &bytes.Buffer{})
-		if err != nil {
-			t.Fatal(err)
-		}
-		w := httptest.NewRecorder()
 
-		prh.Create(w, r)
+		Reset(func() {
+			prdb.entries = nil
+		})
 
-		if w.Code != 400 {
-			t.Errorf("Did not receive a 400 for empty request: %v", w.Code)
-		}
+		Convey("When given a good record", func() {
+			r, err := http.NewRequest("POST", "http://localhost:8080/prereg", &goodRecordBody)
+			if err != nil {
+				t.Fatal(err)
+			}
+			w := httptest.NewRecorder()
+
+			prh.Create(w, r)
+
+			Convey("Should receive back a 201 status code", func() {
+				So(w.Code, ShouldEqual, 201)
+			})
+
+			Convey("Should be inserted into the database", func() {
+				So(len(prdb.entries), ShouldEqual, 1)
+				So(prdb.entries[0], ShouldResemble, goodRecord)
+			})
+		})
+
+		Convey("When given an empty body", func() {
+			r, err := http.NewRequest("POST", "http://localhost:8080/prereg", &bytes.Buffer{})
+			if err != nil {
+				t.Fatal(err)
+			}
+			w := httptest.NewRecorder()
+
+			prh.Create(w, r)
+
+			Convey("Should receive back a 400 code", func() {
+				So(w.Code, ShouldEqual, 400)
+			})
+			Convey("Shouldn't update the database", func() {
+				So(prdb.entries, ShouldBeEmpty)
+			})
+		})
 	})
 }
