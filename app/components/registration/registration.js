3b8ed4027a817513270a1d13cfe1c5b1e41ec86c
Matthew Dawson <matthew@mjdsystems.ca>
2015-01-11 22:19:14 -0500

    Create initial registration service.
    Mock tested as well.  Appears to do something.

--- app/components/registration/registration.js
+++ app/components/registration/registration.js
@@ -1,6 +1,31 @@
 'use strict';
 
 angular.module('ccj16reg.registration', [])
-.factory('registration', function() {
-	return {}
-});
+.factory('registration', ['$q', '$http', function($q, $http) {
+	return {
+		new: function() {
+			return {
+				$$unsaved: true,
+
+				save: function() {
+					var obj = this;
+					if (this.$$unsaved) {
+						return $q(function(resolve, reject) {
+							$http.post('/api/preregistration', angular.toJson(obj)).success(function(data, status) {
+								if (status == 201) {
+									resolve();
+								} else {
+									reject("Failed to create object.");
+								}
+							}).error(function() {
+								reject("Failed to save object.");
+							})
+						})
+					} else {
+						console.log("IMPLEMENT OTHER SAVE");
+					}
+				},
+			}
+		}
+	}
+}]);
--- app/components/registration/registration_test.js
+++ app/components/registration/registration_test.js
@@ -1,11 +1,44 @@
 'use strict';
 
 describe('ccj16reg.registration module', function() {
-  beforeEach(module('ccj16reg.registration'));
+	var $httpBackend, registration;
 
-  describe('version service', function() {
-    it('should return current version', inject(function(registration) {
-      
-    }));
-  });
+	beforeEach(module('ccj16reg.registration'));
+
+	beforeEach(inject(function($injector, _registration_) {
+		$httpBackend = $injector.get('$httpBackend');
+
+		registration = _registration_;
+	}))
+
+	afterEach(function() {
+		$httpBackend.verifyNoOutstandingExpectation();
+		$httpBackend.verifyNoOutstandingRequest();
+	});
+
+	describe('registration service', function() {
+		describe('registration model', function() {
+			it('should give a useful model', function() {
+				var newReg = registration.new();
+				expect(newReg).toBeDefined();
+				expect(newReg.save).not.toBeNull();
+			});
+
+			it('that will save itself to a new object on save()', function() {
+				var newReg = registration.new();
+				newReg.council = "A council";
+				newReg.groupName = "4th Group";
+				newReg.packName = "Pack G";
+				newReg.contactLeaderEmail = "test@example.com";
+
+				$httpBackend.expectPOST('/api/preregistration', angular.toJson(newReg)).respond(201, '');
+
+				newReg.save().then(function() {}, function(message) {
+					console.log(message);
+					expect(false).toBe(true);
+				});
+				$httpBackend.flush()
+			});
+		});
+	});
 });
