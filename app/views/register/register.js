834ecf9d69b98e13c33131bc4ebeca89e2f26e08
Matthew Dawson <matthew@mjdsystems.ca>
2015-01-13 22:08:48 -0500

    Add confirmation fields to the register form for email, and help text.
    Ensure that user consent to email them about this event is received.  Also
    add some other help text as well.

--- app/views/register/register.js
+++ app/views/register/register.js
@@ -12,6 +12,18 @@ angular.module('ccj16reg.view.register', ['ngRoute', 'ngMaterial', 'ccj16reg.reg
 .controller('RegisterCtrl', ['$scope', '$location', '$mdDialog', 'registration', function($scope, $location, $mdDialog, registration) {
 	$scope.registration = new registration();
 
+	$scope.showEmailTos = function(ev) {
+		ev.preventDefault()
+		ev.stopPropagation();
+		$mdDialog.show(
+			$mdDialog.alert()
+				.title('Email usage agreement')
+				.content('All email addresses received through the registration process have been  added to the CCJ16  registration database as well as the CCJ16  mailing list.  During the coming months, you will receive mails from time to time with information about our upcoming CCJ, planning for attending the camp, and other information related to this camp.  Your address will not be distributed to others, nor used for matters not directly connected with CCJ16.  Should you wish to be removed from this distribution list, please send an email to info@cubjamboree.ca')
+				.ok('Done')
+				.targetEvent(ev)
+		);
+	}
+
 	$scope.submitRegistration = function(ev) {
 		$mdDialog.show({
 			templateUrl: 'views/register/pending_submit.html',
