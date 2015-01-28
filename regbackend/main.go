a4047128264135384408ec2f72bbd1c35001f4a7
Matthew Dawson <matthew@mjdsystems.ca>
2015-01-28 00:08:39 -0500

    Add proper configuration.
    Now configuration items can be passed in by a file or parameters to the
    program.  Useful defaults are provided when not specific to CCJ16.
    
    Also Token is now fixed when given!

--- regbackend/main.go
+++ regbackend/main.go
@@ -12,7 +12,33 @@ import (
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/spacemonkeygo/errors/errhttp"
+	"github.com/spacemonkeygo/flagfile"
+	goflagutils "github.com/spacemonkeygo/flagfile/utils"
 )
+
+var httpConfig struct {
+	Listen string `default:":8080" usage:"Address for server to listen on"`
+}
+
+var emailConfig struct {
+	FromAddress  string `default:"no-reply@invalid" usage:"From address for use in emails"`
+	FromName     string `usage:"From name for use in emails"`
+	ContactEmail string `default:"info@invalid" "usage:"Contact email address for use in emails"`
+	Server       string `default:"localhost:25" usage:"Server to use for sending messages"`
+}
+
+var generalConfig struct {
+	Domain              string `default:"invalid" usage:"Domain for use in emails, etc to link people to"`
+	Database            string `default:"records.bolt" usage:"Location to store the database"`
+	AccessToken         string `usage:"Token to access database.  Generated randomly and printed if not set"`
+	StaticFilesLocation string `default:"../app" usage:"Location of static files for the site"`
+}
+
+func init() {
+	goflagutils.Setup("http", &httpConfig)
+	goflagutils.Setup("email", &emailConfig)
+	goflagutils.Setup("", &generalConfig)
+}
 
 type requestLogger struct {
 	H http.Handler
@@ -58,10 +84,12 @@ func httpError(w http.ResponseWriter, err error) {
 }
 
 func main() {
+	flagfile.Load()
+
 	r := mux.NewRouter()
 	apiR := r.PathPrefix("/api/").Subrouter()
 
-	db, err := bolt.Open("records.bolt", 0600, &bolt.Options{Timeout: 1})
+	db, err := bolt.Open(generalConfig.Database, 0600, &bolt.Options{Timeout: 1})
 	if err != nil {
 		log.Fatalf("Failed to open bolt database, err: %s", err)
 	}
@@ -71,30 +99,31 @@ func main() {
 		log.Fatalf("Failed to get group preregistration database started, err %s", err)
 	}
 
-	ces := NewConfirmationEmailService("registration.cubjamboree.ca", "no-reply@cubjamboree.ca", "CCJ16 Registration", "info@cubjamboree.ca", NewLocalMailder("localhost:25"), gprdb)
+	ces := NewConfirmationEmailService(generalConfig.Domain, emailConfig.FromAddress, emailConfig.FromName, emailConfig.ContactEmail, NewLocalMailder(emailConfig.Server), gprdb)
 
 	NewGroupPreRegistrationHandler(apiR, gprdb, ces)
 
-	var key string
-	{
+	key := generalConfig.AccessToken
+	if key == "" {
 		var random [32]byte
 		if _, err := rand.Read(random[:]); err != nil {
 			log.Fatalf("During startup, failed to get entropy with error %s", err)
 		}
 		key = base64.URLEncoding.EncodeToString(random[:])
+		log.Print("DB Token: ", key)
 	}
-	log.Print("DB Token: ", key)
 	apiR.Handle("/grabdb", &grabDb{db}).Headers("X-My-Auth-Token", key).Methods("GET").Queries("key", key)
 
 	http.Handle("/api/", r)
-	otherFiles := http.FileServer(http.Dir("../app"))
+	otherFiles := http.FileServer(http.Dir(generalConfig.StaticFilesLocation))
 	http.Handle("/app.css", otherFiles)
 	http.Handle("/app.js", otherFiles)
 	http.Handle("/components/", otherFiles)
 	http.Handle("/views/", otherFiles)
 	http.Handle("/bower_components/", otherFiles)
+	indexLocation := generalConfig.StaticFilesLocation + "/index.html"
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "../app/index.html")
+		http.ServeFile(w, r, indexLocation)
 	})
-	panic(http.ListenAndServe(":8080", handlers.CompressHandler(&requestLogger{http.DefaultServeMux})))
+	panic(http.ListenAndServe(httpConfig.Listen, handlers.CompressHandler(&requestLogger{http.DefaultServeMux})))
 }
