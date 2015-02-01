540a86f506a99b68057bd6a72f3a5c3596b934d1
Matthew Dawson <matthew@mjdsystems.ca>
2015-02-01 02:57:31 -0500

    Add initial invoice system.
    Add initial system to store invoices.  It just acts like a dumb storage block,
    and piggy backs on other transactions even.

---
+++ regbackend/invoice.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/binary"
+	"time"
+
+	"github.com/CCJ16/registration/regbackend/boltorm"
+)
+
+type Invoice struct {
+	Id        uint64
+	To        string
+	LineItems []InvoiceItem
+	Created   time.Time
+}
+
+type InvoiceItem struct {
+	Description string
+	UnitPrice   int64
+	Count       int64
+}
+
+type InvoiceDb interface {
+	NewInvoice(in *Invoice, tx boltorm.Tx) error
+	GetInvoice(invoiceId uint64, tx boltorm.Tx) (*Invoice, error)
+}
+
+type invoiceDb struct {
+}
+
+var (
+	BOLT_INVOICEBUCKET = []byte("BUCKET_INVOICES")
+)
+
+func NewInvoiceDb(db boltorm.DB) InvoiceDb {
+	if err := db.Update(func(tx boltorm.Tx) error {
+		return tx.CreateBucketIfNotExists(BOLT_INVOICEBUCKET)
+	}); err != nil {
+		panic(err)
+	}
+	return &invoiceDb{}
+}
+
+func (i *invoiceDb) NewInvoice(in *Invoice, tx boltorm.Tx) error {
+	in.Id = 0
+	for in.Id == 0 {
+		id, err := tx.NextSequenceForBucket(BOLT_INVOICEBUCKET)
+		if err != nil {
+			return err
+		}
+		in.Id = id
+	}
+	in.Created = time.Now()
+	var idBytes [8]byte
+	binary.BigEndian.PutUint64(idBytes[:], in.Id)
+	return tx.Insert(BOLT_INVOICEBUCKET, idBytes[:], in)
+}
+
+func (i *invoiceDb) GetInvoice(invoiceId uint64, tx boltorm.Tx) (inv *Invoice, err error) {
+	inv = &Invoice{}
+	var key [8]byte
+	binary.BigEndian.PutUint64(key[:], invoiceId)
+	if err = tx.Get(BOLT_INVOICEBUCKET, key[:], inv); err != nil {
+		if boltorm.ErrKeyDoesNotExist.Contains(err) {
+			return nil, RecordDoesNotExist.New("Could not find invoice")
+		} else {
+			return nil, err
+		}
+	}
+	return inv, nil
+}
---
+++ regbackend/invoice_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"time"
+
+	"github.com/CCJ16/registration/regbackend/boltorm"
+
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestInvoiceStorage(t *testing.T) {
+	Convey("With a valid invoice system", t, func() {
+		db := boltorm.NewMemoryDB()
+		invDb := NewInvoiceDb(db)
+
+		Convey("Requesting a new invoice should succeed", func() {
+			invoice := Invoice{
+				To: "Test group of Test Council",
+				LineItems: []InvoiceItem{
+					InvoiceItem{
+						Description: "Item 1",
+						UnitPrice:   1034,
+						Count:       3,
+					},
+					InvoiceItem{
+						Description: "Item 2",
+						UnitPrice:   60,
+						Count:       1000,
+					},
+				},
+			}
+			err := db.Update(func(tx boltorm.Tx) error {
+				return invDb.NewInvoice(&invoice, tx)
+			})
+			So(err, ShouldBeNil)
+			Convey("And requesting it again by id should succeed", func() {
+				var dbInv *Invoice
+				err := db.View(func(tx boltorm.Tx) error {
+					var err error
+					dbInv, err = invDb.GetInvoice(invoice.Id, tx)
+					return err
+				})
+				So(err, ShouldBeNil)
+				Convey("And the db invoice should be equivalent", func() {
+					So(dbInv.Created, ShouldHappenWithin, 0*time.Second, invoice.Created)
+					dbInv.Created = invoice.Created
+					So(*dbInv, ShouldResemble, invoice)
+				})
+			})
+		})
+	})
+}
