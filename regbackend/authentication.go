b0b06b741702d9a9b62570526789e5461cc3e6b8
Matthew Dawson <matthew@mjdsystems.ca>
2015-02-08 00:23:31 -0500

    Add an api endpoint to check if a session is logged in.
    Watching redirects bugged me, so implements this now, not later.  This pulls
    some common test parts to a separate function.  Also add tests for the new
    endpoint, along with the common part.

--- regbackend/authentication.go
+++ regbackend/authentication.go
@@ -27,6 +27,24 @@ type AuthenticationHandler struct {
 	store sessions.Store
 }
 
+func (a *AuthenticationHandler) sessionIsLoggedin(r *http.Request) bool {
+	sess, _ := sessions.GetRegistry(r).Get(a.store, globalSessionName)
+	if sess == nil || sess.Values[authStatusLoggedIn] == nil || !sess.Values[authStatusLoggedIn].(bool) {
+		return false
+	} else {
+		return true
+	}
+}
+
+func (a *AuthenticationHandler) VerifySession(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	if a.sessionIsLoggedin(r) {
+		w.Write([]byte("true"))
+	} else {
+		w.Write([]byte("false"))
+	}
+}
+
 func (a *AuthenticationHandler) VerifyGoogleToken(w http.ResponseWriter, r *http.Request) {
 	var code string
 	if codeB, err := ioutil.ReadAll(r.Body); err != nil {
@@ -91,9 +109,7 @@ func (a *AuthenticationHandler) VerifyGoogleToken(w http.ResponseWriter, r *http
 
 func (a *AuthenticationHandler) AdminFunc(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		sess, err := sessions.GetRegistry(r).Get(a.store, globalSessionName)
-		if sess == nil || sess.Values[authStatusLoggedIn] == nil || !sess.Values[authStatusLoggedIn].(bool) {
-			log.Print("Failed to get session, err %s", err)
+		if !a.sessionIsLoggedin(r) {
 			http.Error(w, "Forbidden", http.StatusForbidden)
 		} else {
 			h(w, r)
@@ -106,6 +122,7 @@ func NewAuthenticationHandler(r *mux.Router, store sessions.Store) *Authenticati
 		store: store,
 	}
 
+	r.HandleFunc("/authentication/isLoggedIn", authHandler.VerifySession).Methods("GET")
 	r.HandleFunc("/authentication/googletoken", authHandler.VerifyGoogleToken).Methods("POST")
 
 	return authHandler
---
+++ regbackend/authentication_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/gorilla/sessions"
+
+	"testing"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestVerifySession(t *testing.T) {
+	Convey("With initialized pieces", t, func() {
+		r, err := http.NewRequest("GET", "http://localhost:8080/api/authentication/isLoggedIn", nil)
+		So(err, ShouldBeNil)
+		store := sessions.NewCookieStore([]byte("A"))
+		sess, err := sessions.GetRegistry(r).Get(store, globalSessionName)
+		So(err, ShouldBeNil)
+		So(sess, ShouldNotBeNil)
+		aH := AuthenticationHandler{store}
+		helper := func(output string) {
+			w := httptest.NewRecorder()
+			aH.VerifySession(w, r)
+			w.Flush()
+			So(w.HeaderMap.Get("Content-Type"), ShouldEqual, "text/plain")
+			body, err := ioutil.ReadAll(w.Body)
+			So(err, ShouldBeNil)
+			So(string(body), ShouldResemble, output)
+		}
+		Convey("Succeed when session is marked as true", func() {
+			sess.Values[authStatusLoggedIn] = true
+			helper("true")
+		})
+		Convey("Fail when session is marked as false", func() {
+			sess.Values[authStatusLoggedIn] = false
+			helper("false")
+		})
+		Convey("Fail when session is missing the value", func() {
+			delete(sess.Values, authStatusLoggedIn)
+			helper("false")
+		})
+	})
+}
