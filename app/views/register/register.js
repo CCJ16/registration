5ac66d15d5ba1e06edb91602c30d87288d3e3bad
Matthew Dawson <matthew@mjdsystems.ca>
2015-09-15 16:50:03 -0400

    Upgrade user facing packages.
    New angular version pulled in, with some changes in angular material requiring
    less code from me for customization!

--- app/views/register/register.js
+++ app/views/register/register.js
@@ -24,22 +24,6 @@ angular.module("ccj16reg.view.register", ["ngRoute", "ngSanitize", "ngMaterial",
 			.content("All email addresses received through the registration process have been  added to the <span class=\"title-text\">CCJ'16</span>  registration database as well as the <span class=\"title-text\">CCJ'16</span>  mailing list.  During the coming months, you will receive mails from time to time with information about our upcoming CCJ, planning for attending the camp, and other information related to this camp.  Your address will not be distributed to others, nor used for matters not directly connected with <span class=\"title-text\">CCJ'16</span>.  Should you wish to be removed from this distribution list, please send an email to info@cubjamboree.ca")
 			.ok("Done")
 			.targetEvent(ev)
-		dialog._options.template = [
-			"<md-dialog aria-label=\"{{ dialog.ariaLabel }}\">",
-			"<md-content>",
-				"<h2>{{ dialog.title }}</h2>",
-				"<p><span ng-bind-html=\"dialog.content\"></span></p>",
-			"</md-content>",
-			"<div class=\"md-actions\">",
-				"<md-button ng-if=\"dialog.$type == 'confirm'\" ng-click=\"dialog.abort()\">",
-				"{{ dialog.cancel }}",
-				"</md-button>",
-				"<md-button ng-click=\"dialog.hide()\" class=\"md-primary\">",
-				"{{ dialog.ok }}",
-				"</md-button>",
-			"</div>",
-			"</md-dialog>",
-		].join("")
 		$mdDialog.show(dialog);
 	}
 
