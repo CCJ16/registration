9c8371b6d4fe52d440449ab1770332cb979f7cb1
Matthew Dawson <matthew@mjdsystems.ca>
2015-10-03 23:09:17 -0400

    Add waiting list and registered group pages, and sort the waiting list.

--- app/views/recordlist/recordlist.js
+++ app/views/recordlist/recordlist.js
@@ -4,14 +4,38 @@ angular.module("ccj16reg.view.recordlist", ["ngRoute", "ccj16reg.registration", 
 	"use strict";
 	$routeProvider.when("/admin/recordlist", {
 		templateUrl: "views/recordlist/list.html",
-		controller: "RecordListCtrl",
+		controller: "AllRecordListCtrl",
+		resolve: {
+			checkAuth: resolveLoginRequired,
+		},
+	});
+	$routeProvider.when("/admin/registeredlist", {
+		templateUrl: "views/recordlist/list.html",
+		controller: "RegisteredRecordListCtrl",
+		resolve: {
+			checkAuth: resolveLoginRequired,
+		},
+	});
+	$routeProvider.when("/admin/waitinglist", {
+		templateUrl: "views/recordlist/waitinglist.html",
+		controller: "WaitingRecordListCtrl",
 		resolve: {
 			checkAuth: resolveLoginRequired,
 		},
 	});
 })
 
-.controller("RecordListCtrl", function($scope, Registration) {
+.controller("AllRecordListCtrl", function($scope, Registration) {
 	"use strict";
-	$scope.registrations = Registration.query();
+	$scope.registrations = Registration.query({select: "all"});
+})
+
+.controller("RegisteredRecordListCtrl", function($scope, Registration) {
+	"use strict";
+	$scope.registrations = Registration.query({select: "registered"});
+})
+
+.controller("WaitingRecordListCtrl", function($scope, Registration) {
+	"use strict";
+	$scope.registrations = Registration.query({select: "waiting"});
 });
