5db87f45ad32445546b650dacdfc16d1c15395b5
Matthew Dawson <matthew@mjdsystems.ca>
2015-12-06 14:36:12 -0500

    Poll the /config endpoint on app load and inject into application
    On start use some handwritten javascript to poll the /config endpoint, and
    inject into application before bootstrap.

--- app/app/app_test.js
+++ app/app/app_test.js
@@ -1,5 +1,9 @@
 "use strict";
 
+beforeEach(function() {
+	angular.module("ccj16reg.config", []).constant("Config", {})
+})
+
 describe("ccj16reg module", function() {
 	it("should initialize", function() {
 		expect(angular.module("ccj16reg")).toBeTruthy()
