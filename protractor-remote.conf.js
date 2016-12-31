cee1bc36ab83de27ce8e4809efd2c1f23b93195b
Matthew Dawson <matthew@mjdsystems.ca>
2016-12-31 04:14:59 -0500

    Update browser versions
    Fingers crossed that it just works

--- protractor-remote.conf.js
+++ protractor-remote.conf.js
@@ -13,9 +13,9 @@ exports.config = {
 	],
 
 	multiCapabilities: [
-		addCommon({browserName: "chrome", version: 46}),
-		addCommon({browserName: "firefox", version: 42}),
-		addCommon({browserName: "internet explorer", version: 11}),
+		addCommon({browserName: "chrome", version: "latest"}),
+		addCommon({browserName: "firefox", version: "latest"}),
+		addCommon({browserName: "internet explorer", version: "latest"}),
 		addCommon({browserName: "safari", version: "9.0", platform: "OS X 10.11"}),
 	],
 
